24905d6104f6340fbfda382fa7a9af029228ea04
mica <mica_greis@hotmail.com>
2022-09-18 15:34:25 -0300

    Update scripts.js

--- common-js/scripts.js
+++ common-js/scripts.js
@@ -50,7 +50,7 @@
 function countdownTime(){
 	
 	if(isExists('#clock')){
-		$('#clock').countdown('2021/11/22', function(event){
+		$('#clock').countdown('2022/11/06', function(event){
 			var $this = $(this).html(event.strftime(''
 				+ '<div class="time-sec"><span class="title">%D</span> days </div>'
 				+ '<div class="time-sec"><span class="title">%H</span> hours </div>'
